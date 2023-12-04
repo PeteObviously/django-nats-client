748fd4715faf7983c2ed25b5d07d0965b1412856
Pete Dermott <peter@iaido.ai>
2023-12-04 12:02:27 +0000

    Remove requirement for args

--- nats_client/clients.py
+++ nats_client/clients.py
@@ -1,4 +1,4 @@
-__all__ = ['request', 'request_sync', 'publish', 'publish_sync', 'js_publish', 'js_publish_sync']
+__all__ = ["request", "request_sync", "publish", "publish_sync", "js_publish", "js_publish_sync"]
 
 import asyncio
 import functools
@@ -19,24 +19,24 @@ async def get_nc_client(nc: Client = None):
     if nc is None:
         nc = Client()
 
-    server = getattr(settings, 'NATS_SERVER', None)
-    servers = [server] if server else getattr(settings, 'NATS_SERVERS', [])
-    options = getattr(settings, 'NATS_OPTIONS', {})
+    server = getattr(settings, "NATS_SERVER", None)
+    servers = [server] if server else getattr(settings, "NATS_SERVERS", [])
+    options = getattr(settings, "NATS_OPTIONS", {})
 
     await nc.connect(servers=servers, **options)
     return nc
 
 
 async def request(
-        namespace: str, method_name: str, *args, _timeout: float = None, _raw=False, **kwargs
+    namespace: str, method_name: str, *args, _timeout: float = None, _raw=False, **kwargs
 ) -> ResponseType:
     payload = parse_arguments(args, kwargs)
 
     nc = await get_nc_client()
 
-    timeout = _timeout or getattr(settings, 'NATS_REQUEST_TIMEOUT', DEFAULT_REQUEST_TIMEOUT)
+    timeout = _timeout or getattr(settings, "NATS_REQUEST_TIMEOUT", DEFAULT_REQUEST_TIMEOUT)
     try:
-        response = await nc.request(f'{namespace}.{method_name}', payload, timeout=timeout)
+        response = await nc.request(f"{namespace}.{method_name}", payload, timeout=timeout)
     finally:
         await nc.close()
 
@@ -44,35 +44,33 @@ async def request(
     parsed = json.loads(data)
 
     if _raw:
-        parsed.pop('pickled_exc', None)
+        parsed.pop("pickled_exc", None)
         return parsed
 
-    if not parsed['success']:
+    if not parsed["success"]:
         try:
-            exc = jsonpickle.decode(parsed['pickled_exc'])
+            exc = jsonpickle.decode(parsed["pickled_exc"])
         except TypeError:
-            exc = NatsClientException(parsed['error'] + ': ' + parsed['message'])
+            exc = NatsClientException(parsed["error"] + ": " + parsed["message"])
 
         raise exc
 
-    return parsed['result']
+    return parsed["result"]
 
 
 def request_sync(*args, **kwargs):
     return asyncio.run(request(*args, **kwargs))
 
 
-async def publish(namespace: str, method_name: str, *args, _js=False, **kwargs) -> None:
-    payload = parse_arguments(args, kwargs)
-
+async def publish(namespace: str, method_name: str, payload, _js=False) -> None:
     nc = await get_nc_client()
 
     try:
         if _js:
             js = nc.jetstream()
-            await js.publish(f'{namespace}.js.{method_name}', payload)
+            await js.publish(f"{namespace}.js.{method_name}", payload)
         else:
-            await nc.publish(f'{namespace}.{method_name}', payload)
+            await nc.publish(f"{namespace}.{method_name}", payload)
     finally:
         await nc.close()
 
